9325dc10ddfdd4f55883aa4ed37c8bed020ca390
Aarno Aukia <aarno.aukia@vshn.ch>
2018-02-08 15:00:37 +0100

    test mysql server connection

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,31 @@
 # Dockerfile
+
 # inherit from this "empty base image", see https://hub.docker.com/_/python/
 FROM python:alpine
+
 MAINTAINER Aarno Aukia <aarno.aukia@vshn.ch>
+
 WORKDIR /usr/src/app
+
+# install postgresql libs for psycopg2
+RUN apk update && \
+ apk add --virtual .build-deps gcc python3-dev musl-dev postgresql-dev postgresql-libs
+
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
+
+# and remove the libs again to make the image smaller
+RUN apk --purge del .build-deps
+
+# copy application source code into container
 COPY . .
+
 # run this command at run-time
 CMD [ "python", "app.py" ]
+
 # expose this TCP-port
 EXPOSE 3000
+
 # make this path persistent between versions of the container - not needed in this example
 # VOLUME /usr/src/app/mypersistentdatavolume/
 
--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
 import os
+import pymysql
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     result = "<p><b>Hello World!</b></p><p>This is {host}</p><p>The message is {message}</p>".format(host=os.environ.get('HOSTNAME','localhost'), message=os.environ.get('MESSAGE','none'))
+
+    mysqlconn = pymysql.connect(host=os.environ.get('AMAZEEIO_DB_HOST'), port=int(os.environ.get('AMAZEEIO_DB_PORT')), user=os.environ.get('AMAZEEIO_DB_USERNAME'), passwd=os.environ.get('AMAZEEIO_DB_PASSWORD'), db=os.environ.get('AMAZEEIO_SITENAME'))
+    mysqlcursor = mysqlconn.cursor()
+    mysqlcursor.execute('SELECT 1;')
+    for row in mysqlcursor:
+        result += "<p>MySQL: {0}".format(row)
+
     for env in os.environ:
         result += "<p>{0}={1}</p>".format(env,os.environ.get(env))
     return result
