dea794382104763f27cc60d14931a5209345d742
Aarno Aukia <aarno.aukia@vshn.ch>
2018-02-08 11:03:29 +0100

    add more services

--- app.py
+++ app.py
@@ -1,29 +1,13 @@
-import textwrap,os
+from flask import Flask
+import os
+app = Flask(__name__)
 
-from six.moves.BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+@app.route("/")
+def hello():
+    result = "<p><b>Hello World!</b></p><p>This is {host}</p><p>The message is {message}</p>".format(host=os.environ.get('HOSTNAME','localhost'), message=os.environ.get('MESSAGE','none'))
+    for env in os.environ:
+        result += "<p>{0}={1}</p>".format(env,os.environ.get(env))
+    return result
 
-class HelloRequestHandler(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        if self.path != '/':
-            self.send_error(404, "Object not found")
-            return
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html; charset=utf-8')
-        self.end_headers()
-        response_text = textwrap.dedent('''\
-            <html>
-            <head>
-                <title>Greetings to the world</title>
-            </head>
-            <body>
-                <h1>Greetings to the world from {0}</h1>
-                <p>{1}</p>
-            </body>
-            </html>
-        '''.format(os.environ.get('HOSTNAME', 'here'), os.environ.get('MESSAGE', 'Hello, world!')))
-        self.wfile.write(response_text.encode('utf-8'))
-
-server_address = ('', 3000)
-httpd = HTTPServer(server_address, HelloRequestHandler)
-httpd.serve_forever()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=3000)
